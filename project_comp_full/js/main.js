75739c2a343453ffa0b1ed0b271cf255f67e4872
akaFintior <akovaliov.91@gmail.com>
2019-06-17 11:57:24 -0400

    lesson-6 error component added

--- project_comp_full/js/main.js
+++ project_comp_full/js/main.js
@@ -2,11 +2,14 @@ const API = `https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 
 const app = new Vue({
     el: '#app',
+    data: {
+        error: null
+    },
     methods: {
         getJson(url){
             return fetch(url)
                 .then(result => result.json())
-                .catch(error => console.log(error))
+                .catch(error => this.error = error)
         },
     }
 })
