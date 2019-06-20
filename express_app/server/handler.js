b9836d86a3b59baed3de23a3951b9d7cf76931e6
akaFintior <akovaliov.91@gmail.com>
2019-06-20 08:56:04 -0400

    lesson-7 stats.json added

--- express_app/server/handler.js
+++ express_app/server/handler.js
@@ -1,5 +1,6 @@
 const cart = require('./cart');
 const fs = require('fs');
+const moment = require('moment');
 
 const actions = {
     add: cart.add,
@@ -17,7 +18,27 @@ let handler = (req, res, action, file) => {
                 if (err) {
                     res.sendStatus(404, JSON.stringify({result: 0, text: err}));
                 } else {
-                    res.send(JSON.stringify({result: 1, text: 'SUCCESS!'}))
+                    fs.readFile('server/db/stats.json', 'utf-8', (err, data) => {
+                        if (err) {
+                            res.sendStatus(404, JSON.stringify({result: 0, text: err}));
+                        } else {
+                            let newData = JSON.parse(data);
+                            let stats = {
+                                time: moment().format('MMMM Do YYYY, h:mm:ss a'),
+                                operation: action,
+                                item: req.body.product_name || +req.params.id
+                            };
+                            newData.push(stats);
+                            newData = JSON.stringify(newData, null, 3);
+                            fs.writeFile('server/db/stats.json', newData, (err) => {
+                                if (err) {
+                                    res.sendStatus(404, JSON.stringify({result: 0, text: err}));
+                                } else {
+                                    res.send(JSON.stringify({result: 1, text: 'SUCCESS!'}))
+                                }
+                            })
+                        }
+                    })
                 }
             })
         }
